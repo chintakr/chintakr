03cf55bb1e58a0e5858b5ac4b922e1eb6a7a40a6
chintakr <41940030+chintakr@users.noreply.github.com>
2018-08-06 13:33:22 +0530

    Create o.java

---
+++ o.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i;
+		Scanner s = new Scanner(System.in);
+		int n1 = s.nextInt();
+		int n2 = s.nextInt();
+		for(i=n1+1;i<n2;i++)
+		{
+			if(i%2!=0)
+			System.out.println(i);
+		}
+          
+             	
+        }     
+	
+}
